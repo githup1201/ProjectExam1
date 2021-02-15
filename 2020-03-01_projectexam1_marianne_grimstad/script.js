3aa6d56b4bd772215b05253c89cb0dff8abc5025
Marianne Grimstad <marianne.grimstad@gmail.com>
2021-02-15 11:25:43 +0100

    Update script.js

--- 2020-03-01_projectexam1_marianne_grimstad/script.js
+++ 2020-03-01_projectexam1_marianne_grimstad/script.js
@@ -8,15 +8,7 @@ function myFunction() {
 }
 
 
-/*(function($){
-        $().timeline({
-        orientation: 	'vertical',
-        issuesSpeed: 	300,
-        datesSpeed: 	100,
-        arrowKeys: 		'true',
-        startAt:		3
-			})
-		});*/
+
 
 /* index page
 ----------------------*/
